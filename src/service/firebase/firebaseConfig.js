2a0479941cb1316de87ac3ab1b0d74c848a1564c
GastonM12 <gaston_mori_96@hotmail.com>
2024-02-09 16:54:27 -0300

    proyecto final

--- src/service/firebase/firebaseConfig.js
+++ src/service/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjjDdy2OIfI8fyC-wxVos3uUiiw8ZBqE4",
-  authDomain: "ecommerce-c1d37.firebaseapp.com",
-  projectId: "ecommerce-c1d37",
-  storageBucket: "ecommerce-c1d37.appspot.com",
-  messagingSenderId: "735238093581",
-  appId: "1:735238093581:web:86434522a838979ded4e44",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
