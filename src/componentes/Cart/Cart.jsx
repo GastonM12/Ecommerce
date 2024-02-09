16d6e1e934008d7971900f91f4f52c45fd66baed
GastonM12 <gaston_mori_96@hotmail.com>
2024-02-09 19:49:47 -0300

    entrega final

--- src/componentes/Cart/Cart.jsx
+++ src/componentes/Cart/Cart.jsx
@@ -4,8 +4,33 @@ import CartItems from "../CartItems/CartItems";
 import Clase from "./Cart.module.css";
 import Boton from "../Boton/Boton";
 import { FiShoppingBag } from "react-icons/fi";
+import Swal from "sweetalert2";
+
 const Cart = () => {
   const { cart, totalQuantity, ClearCart, total, setCart } = UseCart(CartContext);
+
+ const limpiarCarrito = ()=>{
+  Swal.fire({
+    title: "Estas seguro?",
+    text: "Quieres eliminar los productos del carrito?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Eliminar productos",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "¡Eliminado!",
+        text: "Se han eliminado todos los productos",
+        icon: "success",
+      });
+      
+      return setCart([]);
+    }
+  });
+ }
+
   return (
     <div className={Clase.contenedor}>
       {totalQuantity === 0 ? "" : <h1 className={Clase.titulo}>Mis compras</h1>}
@@ -28,7 +53,7 @@ const Cart = () => {
             {" "}
             <Boton greeting={"Terminar Compra"} />{" "}
           </Link>
-          <button className={Clase.limpiarCarrito} onClick={() => setCart([])}>limpiar carrito</button>
+          <button className={Clase.limpiarCarrito} onClick={limpiarCarrito}>limpiar carrito</button>
         </div>
       )}
     </div>
